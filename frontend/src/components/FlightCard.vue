b5ddf878c1b85922d71d20288e33796629d0c4ba
Jose Luna <josluna1098@gmail.com>
2021-06-23 07:14:25 -0500

    result view

---
+++ frontend/src/components/FlightCard.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-card class="mx-auto my-12" max-width="370">
+    <v-card-title>Vuelo en {{ aeroline }}</v-card-title>
+    <v-divider class="mx-4" />
+    <v-card-text>
+      <a>
+        Viaje de {{ duration }} horas con {{ scales }}
+        {{ scales == 1 ? "escala" : "escalas" }} a solo ${{ price }}</a
+      >
+    </v-card-text>
+    <v-card-actions>
+      <v-btn color="deep-purple lighten-2" text> Reservar </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "FlightCard",
+
+  methods: {
+    getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min)) + min;
+    },
+  },
+
+  computed: {
+    aeroline() {
+      return this.$store.state.aerolines[this.getRandomInt(0, 9)];
+    },
+    duration() {
+      return this.getRandomInt(4, 16);
+    },
+    scales() {
+      return this.getRandomInt(1, 5);
+    },
+    price() {
+      return this.getRandomInt(300, 1801);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ frontend/src/views/Result.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col class="mini-banner" cols="12">
+        <v-row>
+          <v-col class="mb-4">
+            <h1 class="display-2 font-weight-bold mb-3">
+              <div>Servicio Ecuatoriano Internacional</div>
+            </h1>
+
+            <p class="subheading font-weight-regular">
+              Vuelos de {{ origin }} a {{ destination }}
+            </p>
+            <br />
+            <p class="subheading font-weight-regular">
+              {{ departure_date }} - {{ return_date }}
+            </p>
+          </v-col>
+        </v-row>
+      </v-col>
+
+      <v-col class="mb-5 col-sm-auto">
+        <FlightCard v-for="i in 5" v-bind:key="i" />
+      </v-col>
+      <v-col class="mb-5 col-sm-auto">
+        <FlightCard v-for="i in 5" v-bind:key="i" />
+      </v-col>
+      <v-col class="mb-5 col-sm-auto">
+        <FlightCard v-for="i in 5" v-bind:key="i" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import FlightCard from "../components/FlightCard.vue";
+
+export default {
+  name: "Result",
+
+  components: {
+    FlightCard,
+  },
+
+  computed: {
+    origin() {
+      return this.$route.query.origin;
+    },
+    destination() {
+      return this.$route.query.destination;
+    },
+    departure_date() {
+      return this.$route.query.departure_date;
+    },
+    return_date() {
+      return this.$route.query.return_date;
+    },
+  },
+};
+</script>
+
+<style>
+.mini-banner {
+  margin-top: 25px;
+}
+</style>
